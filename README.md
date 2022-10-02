e5aecd3bc6f27ded10e0b28e6a1dbc77b89df385
hxcGit <houxc_mail@163.com>
2022-10-03 02:05:28 +0800

    binary search
    Signed-off-by: hxcGit <houxc_mail@163.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LeetCodeColorPen
 leecode exercise notes
+
+LeetCode Index: https://docs.qq.com/sheet/DTWFKd2l4V1hkU2xN?tab=BB08J2
---
+++ Xavier_hou_go/leetcode/editor/en/FindFirstAndLastPositionOfElementInSortedArray_test.go
@@ -0,0 +1,16 @@
+package cn
+
+import (
+    "testing"
+)
+
+func TestFindFirstAndLastPositionOfElementInSortedArray(t *testing.T) {
+   
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func searchRange(nums []int, target int) []int {
+    
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
